55078634438571992df81c07896410df939ae887
Tusar <43194485+Tusar78@users.noreply.github.com>
2020-02-21 11:55:42 +0600

    Js ES6 Version

--- app.js
+++ app.js
@@ -19,3 +19,27 @@ console.log(first([],3));
 console.log(first([7, 9, 0, -2],3));
 console.log(first([7, 9, 0, -2],6));
 console.log(first([7, 9, 0, -2],-3));
+
+//JS ES6 version
+let first1 = (arr, n) => {
+    if (arr == null) {
+        return void 0;
+    }
+
+    if (n == null) {
+        return arr[0]
+    }
+
+    if (n < 0) {
+        return []
+    }
+
+    return arr.slice(0, n)
+}
+
+
+console.log(first1([7, 9, 0, -2]));
+console.log(first1([],3));
+console.log(first1([7, 9, 0, -2],3));
+console.log(first1([7, 9, 0, -2],6));
+console.log(first1([7, 9, 0, -2],-3));
